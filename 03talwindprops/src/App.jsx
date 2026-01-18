007aba676718181288cd1ee7b1cbf09e22e34d07
PriyamKumar8778 <priyamkumar8778@gmail.com>
2026-01-19 00:19:30 +0530

    integrate tailwind

---
+++ 03talwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card.jsx";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Card message="new image" />
+      <Card message="another image" />
+    </>
+  );
+}
+
+export default App;
